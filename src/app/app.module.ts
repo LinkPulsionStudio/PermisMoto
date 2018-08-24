29b83aa5ad3e76369b02d14b912286615c687c3b
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2018-08-25 01:16:10 +0200

    Create login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,54 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
+import {LoginPage} from "../pages/login/login";
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {AngularFireModule} from "angularfire2";
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFirestoreModule} from "angularfire2/firestore";
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDegnjiZdH1heZh34xqART8aklkmRD_u48",
+    authDomain: "permis-moto.firebaseapp.com",
+    databaseURL: "https://permis-moto.firebaseio.com",
+    projectId: "permis-moto",
+    storageBucket: "permis-moto.appspot.com",
+    messagingSenderId: "897469545600"
+  }
+};
 
 @NgModule({
   declarations: [
     MyApp,
+    LoginPage,
     HomePage,
     ListPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    LoginPage,
     HomePage,
     ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFirestoreModule
   ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, Loading, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {AngularFireAuth} from "angularfire2/auth";
+import {auth} from "firebase";
+import {AngularFirestore} from "angularfire2/firestore";
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +18,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private connectLoading: Loading;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private afAuth: AngularFireAuth,
+              private loadingCtrl: LoadingController,
+              private afs: AngularFirestore) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+
+    this.connectLoading = this.loadingCtrl.create({
+      content: 'Connexion en cours...'
+    });
   }
 
+  /**
+   * This method authenticates the user with FB
+   */
+  facebookLogin() {
+    this.connectLoading.present();
+
+    let fbAuthProvider = new auth.FacebookAuthProvider();
+    fbAuthProvider.addScope('email');
+
+    this.afAuth.auth.signInWithPopup(fbAuthProvider).then(res => {
+      console.log(res.user);
+
+      //checking if users exists
+      this.afs.collection('users').doc(res.user.uid).ref.get().then(doc => {
+        if (!doc.exists){
+          //insert user in DB
+          this.afs.collection('users').doc(res.user.uid).set({
+            uid: res.user.uid,
+            email: res.user.email,
+            displayName: res.user.displayName,
+            profilePic: res.user.photoURL
+          });
+        }
+      });
+    }).catch(err => {
+      this.connectLoading.dismiss();
+      console.log(err);
+    });
+  }
 }
